e99ffb27d7ba9eeb5d8b153fa4864d889ab7cc14
viguime <jvictormguimaraes@gmail.com>
2021-04-26 01:41:00 -0300

    frontend

--- iotfrontend/src/App.js
+++ iotfrontend/src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import api from './services/api';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import Header from './components/Header';
+
+function App(){
+    const [tags,setTags] = useState([]);
+
+    useEffect(()=>{
+        api.get('tags').then(response=>{
+            setTags(response.data);
+        });
+    }, []);
+
+
+    return(
+        <>
+            <Header title = "TAGS"/>
+
+
+            <ul>
+                {tags.map(tag =><li key = {tag.title} >{tag.title} trabalhou {tag.time}  hora(s)</li>)}
+            </ul>
+
+        </>
+    );
 }
 
 export default App;
